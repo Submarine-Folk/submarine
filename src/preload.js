57e50662abf523344b1879c1325e381e75b7f45f
An-AngryBear <monahajt@gmail.com>
2018-01-27 10:17:44 -0600

    delete tutorial code

--- src/preload.js
+++ src/preload.js
@@ -3,10 +3,7 @@
 import { game } from './game';
 
 function preload() {
-    game.load.image('Sky', './assets/Sky.jpg');
-    game.load.image('ground', './assets/platform.png');
-    game.load.image('star', './assets/star.png');
-    game.load.spritesheet('dude', './assets/dude.png', 32, 48);
+
 }
 
 export { preload }; 
